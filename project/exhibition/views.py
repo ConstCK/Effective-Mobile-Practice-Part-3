06d71bb1edff06a51fcf32562bb0eaecf0a371a5
Constantin <Constantin_CK@outlook.com>
2025-01-04 17:02:30 +0300

    feat: Add BreedViewSet

--- project/exhibition/views.py
+++ project/exhibition/views.py
@@ -7,7 +7,7 @@ from rest_framework import viewsets, status
 from rest_framework.response import Response
 
 from .models import Dog, Breed
-from .serializers import DogSerializer
+from .serializers import DogSerializer, BreedSerializer
 from .utils import DogsMixin
 
 
@@ -30,6 +30,16 @@ class DogViewSet(viewsets.ModelViewSet, DogsMixin):
         serializer = DogSerializer(queryset, many=True)
 
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        # Получение собаки с указанным id из БД с дополнительными данными
+
+        try:
+            dog = Dog.objects.get(id=kwargs.get('pk'))
+            serializer = DogSerializer(dog)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            raise Http404({'error': f'Животное с указанным id={kwargs.get('pk')} не найдено.'})
 
     def create(self, request, *args, **kwargs):
         # Создание объекта собака в БД
@@ -71,3 +81,63 @@ class DogViewSet(viewsets.ModelViewSet, DogsMixin):
 
         except ObjectDoesNotExist:
             raise Http404(f'Животное с указанным id={kwargs.get('pk')} не найдено.')
+
+
+class BreedViewSet(viewsets.ModelViewSet):
+    """Набор представлений для CRUD запросов к объектам Breed"""
+    serializer_class = BreedSerializer
+    queryset = Breed.objects.all()
+
+    def list(self, request, *args, **kwargs):
+        # Получение списка всех пород собак из БД с дополнительными данными
+        queryset = Breed.objects.all()
+        serializer = BreedSerializer(queryset, many=True)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def retrieve(self, request, *args, **kwargs):
+        # Получение породы собак с указанным id из БД с дополнительными данными
+        try:
+            breed = Breed.objects.get(id=kwargs.get('pk'))
+            serializer = DogSerializer(breed)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            raise Http404({'error': f'Порода собак с указанным id={kwargs.get('pk')} не найдена.'})
+
+    def create(self, request, *args, **kwargs):
+        # Создание объекта порода собаки в БД
+        try:
+            serializer = BreedSerializer(data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response({'message': f'Успешное создание объекта: {serializer.data}'},
+                            status=status.HTTP_201_CREATED)
+        except Exception as error:
+            return Response({'error': f'Ошибка создания объекта: {error}'},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, *args, **kwargs):
+        # Удаление объекта порода собаки с указанным id в БД
+        try:
+            breed = Breed.objects.get(id=kwargs.get('pk'))
+            breed.delete()
+            return Response({'message': f'Порода собак с id={kwargs.get('pk')} была удалена'},
+                            status=status.HTTP_200_OK)
+        except ObjectDoesNotExist:
+            raise Http404(f'Порода собак с указанным id={kwargs.get('pk')} не найдена.')
+
+    def update(self, request, *args, **kwargs):
+        # Обновление объекта порода собаки с указанным id в БД
+        try:
+            breed = Breed.objects.get(id=kwargs.get('pk'))
+            breed_data = request.data.copy()
+            serializer = BreedSerializer(instance=breed, data=breed_data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response({'message': f'Успешное обновление объекта: {serializer.data}'},
+                                status=status.HTTP_201_CREATED)
+            else:
+                return Response({'error': 'Некорректные данные о породе собак'},
+                                status=status.HTTP_400_BAD_REQUEST)
+        except ObjectDoesNotExist:
+            raise Http404(f'Порода собак с указанным id={kwargs.get('pk')} не найдена.')
